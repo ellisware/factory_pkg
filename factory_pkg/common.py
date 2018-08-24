fea51caedd3ac4a5cb841a65ff510375520d8cb9
Mike Ellis <mtcanuck1@gmail.com>
2018-08-24 16:17:28 -0400

    added exception handling in api requests
    Improving fail over by implementing exception handling

--- factory_pkg/common.py
+++ factory_pkg/common.py
@@ -11,12 +11,36 @@ import json
 IP = '192.168.2.241'
 PORT = '8083'
 API = 'field_api/v3'
-
-SITE = "http://192.168.2.241:40001/v1/node"
+DEFAULT_XA_SITE = "http://xa-site/v1"
 
 
 ################################################
-# Helpers
+# Reqeusts
+
+def make_request(url):
+    
+    dictionary = {}
+    valid = False
+    
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        dictionary = json.loads(response.text)
+        valid = True
+    except requests.exceptions.Timeout:
+        # timeout
+        placeholder = 1
+    except requests.exceptions.RequestException as e:
+        # failure
+        placeholder = 1
+    except requests.exceptions.HTTPError as e:
+        # Response Failure 401 etc.
+        placeholder = 1
+    except ValueError as e:
+        # Invalid JSON
+        placeholder = 1
+    
+    return dictionary, valid
 
 def concat_paths(sequence):
         result = []
@@ -35,8 +59,13 @@ def easy_url(path_parts):
     return urlunsplit((scheme, netloc , hpath , query, fragment))
 
 def json_from_path(path):
-    response = requests.get(easy_url(path))
-    return json.loads(response.text)
+    response, valid = make_request(easy_url(path))
+    return response, valid
+
+
+
+################################################
+# Moments
 
 def moments_parser(data, label):
     ret = {}
